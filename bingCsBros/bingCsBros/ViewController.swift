5684a57996d580590f28e875afdc491cfa666890
Jack Curtin <jcurtin4@binghamton.edu>
2021-04-25 22:59:28 -0400

    Start leaderboard

---
+++ bingCsBros/bingCsBros/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  bingCsBros
+//
+//  Created by Jack Curtin on 4/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var mytableview : UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
